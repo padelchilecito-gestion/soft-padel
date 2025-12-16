929b15d0451429643388a011d1a80f4da657ca97
padelclilecito-gestion <padelchilecito@gmail.com>
2025-12-16 14:55:51 -0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        padel: {
+          blue: '#3b82f6',
+          yellow: '#facc15',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
